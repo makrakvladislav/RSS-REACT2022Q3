26d44f6f4686febebfa2e14cc57d229d1402c60c
Vlad <makrak.vladislav@yandex.ru>
2022-09-28 15:39:45 +0300

    feat: add pages main, about, 404

---
+++ my-app/src/pages/404.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          404 page not found :({' '}
+          <Link to="/" className="text-blue-700 font-medium">
+            Go to home page
+          </Link>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
