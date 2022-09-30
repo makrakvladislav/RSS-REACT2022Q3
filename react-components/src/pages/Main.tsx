f351e578b63e0faacb5e1550aeedf603430c5d8f
Vlad <makrak.vladislav@yandex.ru>
2022-10-01 01:02:58 +0300

    feat: add CardList component to Main component

--- react-components/src/pages/Main.tsx
+++ react-components/src/pages/Main.tsx
@@ -1,8 +1,33 @@
+import CardsList from 'components/CardsList';
+import Loader from 'components/UI/Loader/Loader';
+import Search from 'components/UI/Search/Search';
 import React, { Component } from 'react';
+import Data from '../api/api';
+import IMyState from '../interface/IMyState';
 
-class Main extends Component {
+class Main extends Component<Record<string, never>, IMyState> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = {
+      items: [],
+      isFetching: false,
+    };
+  }
+
+  async componentDidMount() {
+    const response = await Data.getData();
+    this.setState({ items: response, isFetching: true });
+  }
+
   render() {
-    return <h1>Main page</h1>;
+    const data = this.state.items;
+    return (
+      <>
+        <h1>Main page</h1>
+        <Search />
+        {this.state.isFetching ? <CardsList items={data} /> : <Loader />}
+      </>
+    );
   }
 }
 
