cda6e6cbe5919dcc59a6746c7969b60bab147f52
Vlad <makrak.vladislav@yandex.ru>
2022-10-01 00:52:31 +0300

    feat: add CardsList component

---
+++ react-components/src/components/CardsList.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Card from './UI/Card/Card';
+import IMyState from '../interface/IMyState';
+
+class CardsList extends Component<IMyState> {
+  state: IMyState = {
+    items: this.props.items,
+  };
+  render() {
+    return (
+      <>
+        <div className="grid mb-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-cols-1 grid-flow-row gap-4">
+          {this.state.items.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </div>
+      </>
+    );
+  }
+}
+
+export default CardsList;
