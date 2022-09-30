a166baaa1054581ce2790b1e2344ecd5c70549bd
Vlad <makrak.vladislav@yandex.ru>
2022-10-01 01:00:28 +0300

    refactor: remove search component from header

--- react-components/src/components/Header.tsx
+++ react-components/src/components/Header.tsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import Menu from './UI/Menu/Menu';
-import Search from './UI/Search/Search';
 
 export class Header extends Component {
   render() {
     return (
       <>
         <header className="header gap-3 p-3 bg-gray-50 rounded border-gray-200 flex flex-wrap justify-between">
-          <Menu></Menu>
-          <Search></Search>
+          <Menu />
         </header>
       </>
     );
