677f8e91302976886c092e2976c815113807e15b
Vlad <makrak.vladislav@yandex.ru>
2022-10-02 02:35:58 +0300

    feat: add tests for Card component

---
+++ react-components/src/components/UI/Card/Card.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Card from './Card';
+
+const data = {
+  albumId: 1,
+  id: 1,
+  title: 'accusamus beatae ad facilis cum similique qui sunt',
+  url: 'https://via.placeholder.com/600/92c952',
+  thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+};
+
+test('renders card component', () => {
+  render(<Card item={data} />);
+  screen.debug();
+  expect(
+    screen.getByText(/accusamus beatae ad facilis cum similique qui sunt/i)
+  ).toBeInTheDocument();
+});
