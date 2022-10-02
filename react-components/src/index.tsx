0c90473ebeddcd428bbc276c9957d69f64e5c099
Vlad <makrak.vladislav@yandex.ru>
2022-10-02 04:07:07 +0300

    fix: router for gh-pages

--- react-components/src/index.tsx
+++ react-components/src/index.tsx
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <HashRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
   </HashRouter>
 );
