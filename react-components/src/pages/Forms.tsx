e3f81d02b5c77ea9dac2860eed66f7c792d6188e
Vlad <makrak.vladislav@yandex.ru>
2022-10-05 19:36:16 +0300

    feat: add Forms page

---
+++ react-components/src/pages/Forms.tsx
@@ -0,0 +1,15 @@
+import Form from 'components/Form/Form';
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <>
+        <h1>Form</h1>
+        <Form />
+      </>
+    );
+  }
+}
+
+export default About;
