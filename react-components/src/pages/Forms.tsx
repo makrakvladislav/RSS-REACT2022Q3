68ccf16abc711afacb7941546d212fefbbe0fca0
Vlad <makrak.vladislav@yandex.ru>
2022-10-10 02:31:40 +0300

    feat: add components form, formCardsList to form page

--- react-components/src/pages/Forms.tsx
+++ react-components/src/pages/Forms.tsx
@@ -1,12 +1,30 @@
 import Form from 'components/Form/Form';
+import FormCardsList from 'components/Form/FormCardsList/FormCardsList';
+import IFormCard from 'interface/IFormCard';
 import React, { Component } from 'react';
+import IFormState from 'interface/IFormState';
 
-class About extends Component {
+class About extends Component<Record<string, never>, IFormState> {
+  state: IFormState = {
+    items: [],
+  };
+
+  constructor(props: Record<string, never>) {
+    super(props);
+  }
+
+  createCard = (item: IFormCard) => {
+    this.setState((prevState) => ({
+      items: prevState.items.concat(item),
+    }));
+  };
+
   render() {
     return (
       <>
         <h1>Form</h1>
-        <Form />
+        <Form handleClick={this.createCard} />
+        <FormCardsList items={this.state.items} />
       </>
     );
   }
