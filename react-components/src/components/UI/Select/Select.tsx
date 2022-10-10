998df134b0fd8077292820d767c79548f73e0c78
Vlad <makrak.vladislav@yandex.ru>
2022-10-10 22:37:52 +0300

    feat: add error field

--- react-components/src/components/UI/Select/Select.tsx
+++ react-components/src/components/UI/Select/Select.tsx
@@ -5,13 +5,20 @@ type Ref = HTMLSelectElement | null;
 
 const Select = React.forwardRef<Ref, ISelectProps>((props, ref) => {
   return (
-    <select
-      ref={ref}
-      {...props}
-      className="w-full mb-2.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5"
-    >
-      <option selected>Choose a country</option>
-      <>
+    <>
+      <select
+        ref={ref}
+        {...props}
+        defaultValue={'default'}
+        className={
+          !props.errorstate
+            ? 'w-full mb-2.5 bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg p-2.5'
+            : 'w-full mb-2.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5'
+        }
+      >
+        <option value="default" disabled selected>
+          Choose a country
+        </option>
         {props.options.map((item: string, key: number) => {
           return (
             <option key={key} value={item}>
@@ -19,8 +26,17 @@ const Select = React.forwardRef<Ref, ISelectProps>((props, ref) => {
             </option>
           );
         })}
-      </>
-    </select>
+      </select>
+      <div
+        className={
+          !props.errorstate
+            ? 'flex mt-1 text-sm text-red-600 opacity-1 transition-opacity duration-300'
+            : 'flex mt-1 text-sm text-red-600 opacity-0 transition-opacity duration-300'
+        }
+      >
+        {props.errormessage}
+      </div>
+    </>
   );
 });
 
