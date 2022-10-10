dd1e72ee5b644ed115b1b0687fdd542bca267abf
Vlad <makrak.vladislav@yandex.ru>
2022-10-10 22:41:30 +0300

    feat: add SelectProps interface

--- react-components/src/interface/ISelectProps.tsx
+++ react-components/src/interface/ISelectProps.tsx
@@ -1,3 +1,7 @@
 export default interface ISelectProps {
   options: Array<string>;
+  name?: string;
+  errorstate?: boolean;
+  errormessage?: string;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
