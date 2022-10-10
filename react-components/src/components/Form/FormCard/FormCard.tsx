3efc32a04796f8bef25ef5091456d89fa09780b9
Vlad <makrak.vladislav@yandex.ru>
2022-10-10 23:06:49 +0300

    feat: output inputs values in formCard

--- react-components/src/components/Form/FormCard/FormCard.tsx
+++ react-components/src/components/Form/FormCard/FormCard.tsx
@@ -11,17 +11,34 @@ export class FormCard extends Component<IForm, IFormCardState> {
     };
   }
   render() {
-    //<imgsrc={`${this.state.item}`}className="w-full rounded-t-lg object-contain"alt="Post img"/>
     return (
       <>
-        <div className="card bg-white rounded-lg border border-gray-200 shadow-md">
-          <div className="p-5">
-            <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-              {this.state.item.name} {this.state.item.lastName}
-            </h3>
-            <span className="m-b-5 text-xs italic text-gray-600">{this.state.item.birthday}</span>
-            <div>{this.state.item.email}</div>
-            <div>{this.state.item.avatar}</div>
+        <div className="card flex flex-col bg-white rounded-lg border border-gray-200 shadow-md">
+          <img
+            src={`${this.state.item.avatar}`}
+            className="w-full h-full max-h-60 rounded-t-lg object-cover"
+            alt="Post img"
+          />
+          <div className="flex flex-col p-5">
+            <div className="mb-2">
+              <h3 className="text-2xl font-bold tracking-tight text-gray-900">
+                {this.state.item.name} {this.state.item.lastName}
+              </h3>
+              <span className="text-sm text-gray-600 italic">{this.state.item.country}</span>
+            </div>
+            <div className="block mb-2 text-xs italic text-gray-600">
+              {this.state.item.birthday}
+            </div>
+            <div className="flex items-center m-b-5 text-sm text-gray-600">
+              <span className="text-sm italic">{this.state.item.email}</span>
+            </div>
+            <div className="flex items-center">
+              <span className="text-sm">
+                {this.state.item.subscribe
+                  ? 'Subscribed to newsletter'
+                  : 'NOT Subscribed to newsletter'}
+              </span>
+            </div>
           </div>
         </div>
       </>
