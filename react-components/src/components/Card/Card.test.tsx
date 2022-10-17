9f967de4d54a5491bf66c7091f74e1b3f45b7d84
Vlad <makrak.vladislav@yandex.ru>
2022-10-17 18:33:41 +0300

    fix: fix test cases for new api

--- react-components/src/components/Card/Card.test.tsx
+++ react-components/src/components/Card/Card.test.tsx
@@ -3,18 +3,25 @@ import { render, screen } from '@testing-library/react';
 import Card from './Card';
 
 const data = {
-  albumId: 1,
-  id: 1,
-  title: 'accusamus beatae ad facilis cum similique qui sunt',
-  url: 'https://via.placeholder.com/600/92c952',
-  thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+  adult: false,
+  backdrop_path: '/5hoS3nEkGGXUfmnu39yw1k52JX5.jpg',
+  genre_ids: [28, 12, 14],
+  id: 960704,
+  original_language: 'ja',
+  original_title: '鋼の錬金術師 完結編 最後の錬成',
+  overview:
+    'The Elric brothers’ long and winding journey comes to a close in this epic finale, where they must face off against an unworldly, nationwide threat.',
+  popularity: 4030.287,
+  poster_path: '/AeyiuQUUs78bPkz18FY3AzNFF8b.jpg',
+  release_date: '2022-06-24',
+  title: 'Fullmetal Alchemist: The Final Alchemy',
+  video: false,
+  vote_average: 6.7,
+  vote_count: 47,
 };
-/*
+
 test('renders card component', () => {
   render(<Card item={data} />);
   screen.debug();
-  expect(
-    screen.getByText(/accusamus beatae ad facilis cum similique qui sunt/i)
-  ).toBeInTheDocument();
+  expect(screen.getByText(/Fullmetal Alchemist: The Final Alchemy/i)).toBeInTheDocument();
 });
-*/
