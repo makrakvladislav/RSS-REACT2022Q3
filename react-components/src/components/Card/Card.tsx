7fb3ba419a366adcaee62f94f6fd6372b1d4ff19
Vlad <makrak.vladislav@yandex.ru>
2022-10-25 01:14:07 +0300

    refactor: refactor to functional components

--- react-components/src/components/Card/Card.tsx
+++ react-components/src/components/Card/Card.tsx
@@ -1,44 +1,33 @@
-import ICardState from '../../interface/ICardState';
 import IMyProps from '../../interface/IMyProps';
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 import ImagePlaceholder from 'components/UI/ImagePlaceholder/ImagePlaceholder';
 
-export class Card extends Component<IMyProps, ICardState> {
-  constructor(props: IMyProps) {
-    super(props);
-    this.state = {
-      item: props.item,
-    };
-  }
-  render() {
-    return (
-      <>
-        <div className="card flex flex-col bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-          {this.state.item.poster_path !== null ? (
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${this.state.item.poster_path}`}
-              className="w-full rounded-t-lg object-contain max-w-md"
-              alt="Post img"
-            />
-          ) : (
-            <ImagePlaceholder />
-          )}
+export default memo(function Card(props: IMyProps) {
+  return (
+    <>
+      <div className="card flex flex-col bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        {props.item.poster_path !== null ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${props.item.poster_path}`}
+            className="w-full rounded-t-lg object-contain max-w-md"
+            alt="Post img"
+          />
+        ) : (
+          <ImagePlaceholder />
+        )}
 
-          <div className="flex flex-col h-full justify-between p-5">
-            <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {this.state.item.title}
-            </h3>
-            <button
-              onClick={() => this.props!.setVisible!(true, this.state.item.id)}
-              className="w-full mt-4 text-white right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-4 py-1"
-            >
-              More info
-            </button>
-          </div>
+        <div className="flex flex-col h-full justify-between p-5">
+          <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {props.item.title}
+          </h3>
+          <button
+            onClick={() => props!.setVisible!(true, props.item.id)}
+            className="w-full mt-4 text-white right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-4 py-1"
+          >
+            More info
+          </button>
         </div>
-      </>
-    );
-  }
-}
-
-export default Card;
+      </div>
+    </>
+  );
+});
--- react-components/src/components/CardsList/CardsList.tsx
+++ react-components/src/components/CardsList/CardsList.tsx
@@ -1,24 +1,15 @@
 import Card from 'components/Card/Card';
 import { ICardListProps } from 'interface/ICardListProps';
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 
-import IMyState from '../../interface/IMyState';
-
-class CardsList extends Component<ICardListProps> {
-  state: IMyState = {
-    items: this.props.items,
-  };
-  render() {
-    return (
-      <>
-        <div className="grid mb-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 grid-cols-1 grid-flow-row gap-4">
-          {this.state.items.map((item, index) => (
-            <Card key={index} item={item} setVisible={this.props.setVisible} />
-          ))}
-        </div>
-      </>
-    );
-  }
-}
-
-export default CardsList;
+export default memo(function CardsList(props: ICardListProps) {
+  return (
+    <>
+      <div className="grid mb-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 grid-cols-1 grid-flow-row gap-4">
+        {props.items.map((item, index) => (
+          <Card key={index} item={item} setVisible={props.setVisible} />
+        ))}
+      </div>
+    </>
+  );
+});
