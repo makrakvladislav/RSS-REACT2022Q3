d327d21d2ab9b2e687057f6230ecd1dbe231096b
Vlad <makrak.vladislav@yandex.ru>
2022-10-25 19:14:51 +0300

    feat: add validation helpers

--- react-components/src/utils/helpers.tsx
+++ react-components/src/utils/helpers.tsx
@@ -1,17 +1,29 @@
-type emailCheck = (val: HTMLInputElement | null | undefined) => boolean;
-type dateCheck = (val: HTMLInputElement | null | undefined) => boolean;
+type emailCheck = (val: string) => boolean;
+type dateCheck = (val: Date) => boolean;
+type nameCheck = (val: string) => boolean;
+type countryCheck = (val: string) => boolean;
 
 const emailValidate: emailCheck = (val) => {
   const regEx = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
-  return regEx.test(val!.value);
+  return regEx.test(val);
 };
 
 const dateValidate: dateCheck = (val) => {
-  const date = Number(new Date(val!.value));
-  const currentDate = Number(new Date());
-  return (
-    (Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date)) || date < currentDate
-  );
+  const date = new Date(val);
+  if (!date) {
+    return false;
+  }
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  return !(date > today);
 };
 
-export { emailValidate, dateValidate };
+const nameValidate: nameCheck = (val) => {
+  return !(val.length < 3);
+};
+
+const contryValidate: countryCheck = (val) => {
+  return val !== 'default';
+};
+
+export { emailValidate, dateValidate, nameValidate, contryValidate };
