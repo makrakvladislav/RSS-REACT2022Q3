27b73d0932db63a51743bdbbbcb969a7b02dda2c
Vlad <makrak.vladislav@yandex.ru>
2022-11-04 21:12:18 +0300

    feat: add global state and pagination for search page component

--- react-components/src/pages/Search.tsx
+++ react-components/src/pages/Search.tsx
@@ -1,64 +1,70 @@
 import Data from 'api/api';
-import { ICard } from 'components/Card/Card';
 import CardsList from 'components/CardsList/CardsList';
 import {
   fetchDataAction,
-  initializePaginationAction,
+  paginationSearchAction,
   saveSearchQueryAction,
 } from 'components/GlobalState/Actions';
-import { useCustomState, useDispatch } from 'components/GlobalState/StateContext';
+import {
+  cachedItemsSelector,
+  limitPerPageSelector,
+  pageCurrentSelector,
+  searchQuerySelector,
+  useCustomState,
+  useDispatch,
+  useSelector,
+} from 'components/GlobalState/StateContext';
 import Loader from 'components/UI/Loader/Loader';
 import Modal from 'components/UI/Modal/Modal';
 import SearchError from 'components/UI/SearchError/SearchError';
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo, useCallback, useEffect, useState } from 'react';
 import SearchInput from 'components/Search/Search';
 import Pagination from 'components/UI/Pagination/Pagination';
 
 const Search = memo(() => {
-  const [items, setItems] = useState<Array<ICard>>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [visibleModalId, setVisibleModalId] = useState<number | null>(null); // modalId === movieId
-
   const dispatch = useDispatch();
   const state = useCustomState();
+  const cachedItems = useSelector(cachedItemsSelector);
+  const currentPageNumber = useSelector(pageCurrentSelector);
+  const searchQuery = useSelector(searchQuerySelector);
+  const limitPerPage = useSelector(limitPerPageSelector);
+  const [isLoading, setIsLoading] = useState(false);
+  const [visibleModalId, setVisibleModalId] = useState<number | null>(null); // modalId === movieId
 
-  const getItems = async () => {
-    const response = await Data.getMovies(1);
-    if (response !== undefined) {
-      setIsLoading(false);
-      if (state.cache.cards.length > 0) {
-        setItems(state.cache.cards);
-      } else {
-        dispatch(fetchDataAction(response.results.results));
-        setItems(response.results.results);
-      }
-    }
-  };
-
-  const searchHandler = async (query: string) => {
+  const getData = async (query: string, page: number, limit: number) => {
     setIsLoading(true);
-    const response = await Data.getByQuery(1, query);
+    const response = await Data.getByQuery(page, query);
     if (response !== undefined) {
-      dispatch(initializePaginationAction(response.results.page, response.results.total_pages));
-
-      dispatch(fetchDataAction(response.results.results));
-      dispatch(saveSearchQueryAction(query));
-      setItems(response.results.results);
+      dispatch(fetchDataAction(response.results.results.slice(0, limit)));
+      dispatch(paginationSearchAction(response.results.page, response.results.total_pages));
       setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    getItems();
-  }, []);
+    getData(searchQuery, currentPageNumber, limitPerPage);
+  }, [searchQuery, currentPageNumber, limitPerPage]);
 
   return (
     <>
       <h1>Search</h1>
-      <SearchInput handleSearch={searchHandler} />
-      {items.length == 0 && !isLoading && <SearchError />}
-      {isLoading ? <Loader /> : <CardsList items={items} setVisible={setVisibleModalId} />}
-      <Pagination pageCount={state.workspace.mainPage.pagination.pagesCount!} />
+      <SearchInput />
+      {cachedItems.length === 0 && !isLoading && <SearchError />}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <CardsList items={cachedItems} setVisible={setVisibleModalId} />
+          {cachedItems.length > 0 && (
+            <Pagination
+              pageType="search"
+              pageCount={state.workspace.searchPage.pagination.pagesCount}
+              currentPage={state.workspace.searchPage.pagination.currentPage}
+            />
+          )}
+        </>
+      )}
+
       {visibleModalId && <Modal movieId={visibleModalId} setVisible={setVisibleModalId} />}
     </>
   );
