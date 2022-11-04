8af11a6fcfb27db5ea59b77ca2e1b60a6ab73132
Vlad <makrak.vladislav@yandex.ru>
2022-11-04 21:08:35 +0300

    feat: add global state and pagination for main page component

--- react-components/src/pages/Main.tsx
+++ react-components/src/pages/Main.tsx
@@ -1,67 +1,71 @@
 import CardsList from '../components/CardsList/CardsList';
 import Loader from '../components/UI/Loader/Loader';
-import Search from '../components/Search/Search';
 import React, { memo, useEffect, useState } from 'react';
 import Data from '../api/api';
 import SearchError from 'components/UI/SearchError/SearchError';
 import Modal from 'components/UI/Modal/Modal';
-import { ICard } from 'components/Card/Card';
-import { useCustomState, useDispatch } from 'components/GlobalState/StateContext';
 import {
-  fetchDataAction,
-  initializePaginationAction,
-  saveSearchQueryAction,
-} from 'components/GlobalState/Actions';
+  cachedItemsSelector,
+  limitPerPageSelector,
+  pageMainCurrentSelector,
+  sortPageSelector,
+  useCustomState,
+  useDispatch,
+  useSelector,
+} from 'components/GlobalState/StateContext';
+import { fetchDataAction, paginationAction } from 'components/GlobalState/Actions';
 import Pagination from 'components/UI/Pagination/Pagination';
+import CatalogSelector from 'components/UI/CatalogSelector/CatalogSelector';
+import { limitOptions, sortOptions } from 'utils/helpers';
 
 const Main = memo(() => {
-  const [items, setItems] = useState<Array<ICard>>([]);
+  const dispatch = useDispatch();
+  const state = useCustomState();
+  const cachedItems = useSelector(cachedItemsSelector);
+  const limitPerPage = useSelector(limitPerPageSelector);
   const [isLoading, setIsLoading] = useState(true);
   const [visibleModalId, setVisibleModalId] = useState<number | null>(null); // modalId === movieId
 
-  const dispatch = useDispatch();
-  const state = useCustomState();
+  const currentPageNumber = useSelector(pageMainCurrentSelector);
+  const sortType = useSelector(sortPageSelector);
 
-  const getItems = async () => {
-    const response = await Data.getMovies(1);
-    console.log(response);
-
-    if (response !== undefined) {
-      setIsLoading(false);
-
-      if (state.cache.cards.length > 0) {
-        setItems(state.cache.cards);
-      } else {
-        dispatch(fetchDataAction(response.results.results));
-        setItems(response.results.results);
-      }
-    }
-  };
-
-  const searchHandler = async (query: string) => {
+  const getData = async (currentPageNumber: number, sortBy: string, limit: number) => {
     setIsLoading(true);
-    const response = await Data.getByQuery(1, query);
+    const response = await Data.getMovies(currentPageNumber, sortBy);
     if (response !== undefined) {
-      dispatch(initializePaginationAction(response.results.page, response.results.total_pages));
-      console.log(response.results.total_pages);
-      dispatch(fetchDataAction(response.results.results));
-      dispatch(saveSearchQueryAction(query));
-      setItems(response.results.results);
+      dispatch(fetchDataAction(response.results.results.slice(0, limit)));
+      dispatch(paginationAction(response.results.page, response.results.total_pages));
       setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    getItems();
-  }, []);
+    getData(currentPageNumber, sortType, limitPerPage);
+  }, [sortType, currentPageNumber, limitPerPage]);
 
   return (
     <>
       <h1>Main</h1>
-      <Search handleSearch={searchHandler} />
-      {items.length == 0 && !isLoading && <SearchError />}
-      {isLoading ? <Loader /> : <CardsList items={items} setVisible={setVisibleModalId} />}
-      <Pagination pageCount={state.workspace.mainPage.pagination.pagesCount!} />
+      <div className="flex gap-2 mb-3 justify-end">
+        <CatalogSelector type="limit" options={limitOptions} />
+        <CatalogSelector type="sort" options={sortOptions} />
+      </div>
+      {cachedItems.length == 0 && !isLoading && <SearchError />}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <CardsList items={cachedItems} setVisible={setVisibleModalId} />
+          {cachedItems.length > 0 && (
+            <Pagination
+              pageType="main"
+              pageCount={300}
+              currentPage={state.workspace.mainPage.pagination.currentPage!}
+            />
+          )}
+        </>
+      )}
+
       {visibleModalId && <Modal movieId={visibleModalId} setVisible={setVisibleModalId} />}
     </>
   );
