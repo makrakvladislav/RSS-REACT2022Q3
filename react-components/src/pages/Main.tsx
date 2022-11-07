fadbed7620e7113b78e0f55c1c4237c19216dcb0
Vlad <makrak.vladislav@yandex.ru>
2022-11-08 01:47:30 +0300

    refactor: add redux store for main page component

--- react-components/src/pages/Main.tsx
+++ react-components/src/pages/Main.tsx
@@ -1,71 +1,44 @@
 import CardsList from '../components/CardsList/CardsList';
 import Loader from '../components/UI/Loader/Loader';
 import React, { memo, useEffect, useState } from 'react';
-import Data from '../api/api';
 import SearchError from 'components/UI/SearchError/SearchError';
 import Modal from 'components/UI/Modal/Modal';
-import {
-  cachedItemsSelector,
-  limitPerPageSelector,
-  currentMainPageSelector,
-  useCustomState,
-  useDispatch,
-  useSelector,
-  sortTypeSelector,
-} from 'components/GlobalState/StateContext';
-import { fetchDataAction, paginationMainAction } from 'components/GlobalState/Actions';
 import Pagination from 'components/UI/Pagination/Pagination';
-import CatalogSelector from 'components/UI/CatalogSelector/CatalogSelector';
 import { limitOptions, sortOptions } from 'utils/helpers';
+import { useAppDispatch, useAppselector } from 'store/hooks/redux';
+import { fetchMovies } from 'store/reducers/ActionCreators';
+import LimitSelector from 'components/UI/LimitSelector/LimitSelector';
+import SortSelector from 'components/UI/SortSelector/SortSelector';
 
 const Main = memo(() => {
-  const dispatch = useDispatch();
-  const state = useCustomState();
-  const cachedItems = useSelector(cachedItemsSelector);
-  const limitPerPage = useSelector(limitPerPageSelector);
-  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useAppDispatch();
+  const { cards, isLoading, currentPage, limit, sortType, error } = useAppselector(
+    (state) => state.movieReducer
+  );
   const [visibleModalId, setVisibleModalId] = useState<number | null>(null); // modalId === movieId
 
-  const currentPage = useSelector(currentMainPageSelector);
-  const sortType = useSelector(sortTypeSelector);
-
-  const getData = async (page: number, sortBy: string, limit: number) => {
-    setIsLoading(true);
-    const response = await Data.getMovies(page, sortBy);
-    if (response !== undefined) {
-      dispatch(fetchDataAction(response.results.results.slice(0, limit)));
-      dispatch(paginationMainAction(response.results.page, response.results.total_pages));
-      setIsLoading(false);
-    }
-  };
-
   useEffect(() => {
-    getData(currentPage, sortType, limitPerPage);
-  }, [sortType, currentPage, limitPerPage]);
+    dispatch(fetchMovies({ currentPage, sortType }));
+  }, [sortType, currentPage, limit]);
 
   return (
     <>
       <h1>Main</h1>
       <div className="flex gap-2 mb-3 justify-end">
-        <CatalogSelector type="limit" options={limitOptions} />
-        <CatalogSelector type="sort" options={sortOptions} />
+        <LimitSelector page="main" options={limitOptions} />
+        <SortSelector options={sortOptions} />
       </div>
-      {cachedItems.length == 0 && !isLoading && <SearchError />}
+      {cards.length == 0 && !isLoading && <SearchError error={error} />}
       {isLoading ? (
         <Loader />
       ) : (
         <>
-          <CardsList items={cachedItems} setVisible={setVisibleModalId} />
-          {cachedItems.length > 0 && (
-            <Pagination
-              pageType="main"
-              pageCount={300}
-              currentPage={state.workspace.mainPage.pagination.currentPage!}
-            />
+          <CardsList items={cards} setVisible={setVisibleModalId} />
+          {cards.length > 0 && (
+            <Pagination pageType="main" pageCount={300} currentPage={currentPage} />
           )}
         </>
       )}
-
       {visibleModalId && <Modal movieId={visibleModalId} setVisible={setVisibleModalId} />}
     </>
   );
